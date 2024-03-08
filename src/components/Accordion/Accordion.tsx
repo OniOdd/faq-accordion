3f4e52914f9c86bc542ded6c736ec57c02997938
Mykyta Kavetskyi <103828218+OniOdd@users.noreply.github.com>
2024-03-08 02:07:52 +0200

    structure is ready

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,8 +1,50 @@
 import style from './Accordion.module.css';
+import data from './data.json';
+import { useState } from 'react';
 
 function Accordion() {
+  const [selected, setSelected] = useState<number[]>([]);
+
+  function handleSelection(id: number) {
+    const choices = [...selected];
+    const findIndexOfCurrentId = choices.indexOf(id);
+
+    if (findIndexOfCurrentId === -1) choices.push(id);
+    else choices.splice(findIndexOfCurrentId, 1);
+
+    setSelected(choices);
+  }
+
+  function keyDownHandler(event: React.KeyboardEvent, id: number) {
+    if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+      event.preventDefault();
+      handleSelection(id);
+    }
+  }
+
   return (
-    <article></article>
+    <article className={style.accordion}>
+      <header className={style.accordion__header}>
+        <span className={style.accordion__icon}></span>
+        <h1 className={style.accordion__title}>FAQs</h1>
+      </header>
+      {
+        data && data.length > 0 && data.map(item => (
+          <section className={`${style.accordion__section} ${style.section}`} key={item.id}>
+            <header className={style.section__header} tabIndex={0} onKeyDown={event => keyDownHandler(event, item.id)}
+                    onClick={() => handleSelection(item.id)}>
+              <h2 className={style.section__question}>{item.question}</h2>
+              {
+                selected.includes(item.id) ?
+                  <span className={style.section__iconMinus}></span> :
+                  <span className={style.section__iconPlus}></span>
+              }
+            </header>
+            {selected.includes(item.id) && <p className={style.section__answer}>{item.answer}</p>}
+          </section>
+        ))
+      }
+    </article>
   );
 }
 
